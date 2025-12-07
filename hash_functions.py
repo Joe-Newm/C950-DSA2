c195fc2e121dfd47d9d8d79aeae3338431c34bdf
joe-newm <j.newm500@gmail.com>
2025-12-06 20:16:57 -0600

    add insert function and hash function to hash table

--- hash_functions.py
+++ hash_functions.py
@@ -1,4 +1,11 @@
-class Hash_Table(object):
+class HashTable:
     def __init__(self, bucket):
         self.__bucket = bucket
         self.__table = [[] for _ in range(bucket)]
+
+    def hash(self, key):
+        return (key % self._bucket)
+    
+    def insertItem(self, key, value):
+        index = self.hash(key)
+        self.__table[index].append((key, value))
