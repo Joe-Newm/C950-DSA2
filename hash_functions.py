3e1a10900d4f3eefc7e854ebbd306e7b5568883b
joe-newm <j.newm500@gmail.com>
2025-12-06 21:36:24 -0600

    add package class

--- hash_functions.py
+++ hash_functions.py
@@ -1,11 +1,18 @@
 class HashTable:
     def __init__(self, bucket):
-        self.__bucket = bucket
-        self.__table = [[] for _ in range(bucket)]
+        self.bucket = bucket
+        self.table = [[] for _ in range(bucket)]
 
     def hash(self, key):
         return (key % self._bucket)
     
     def insertItem(self, key, value):
         index = self.hash(key)
-        self.__table[index].append((key, value))
+        self.table[index].append((key, value))
+
+    def lookUp(self, key):
+        index = self.hash(key)
+        for k, v in self.table[index]:
+            if k == key:
+                return v
+        return None
---
+++ package.py
@@ -0,0 +1,11 @@
+class Package:
+    def __init__(self, id, city, state, zip, deadline, weight, notes, loadingTime, deliveryTime):
+        self.id = id
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deadline = deadline
+        self.weight = weight
+        self.notes = notes
+        self.loadingTime = loadingTime
+        self.deliveryTime = deliveryTime
