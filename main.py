b739c1f1ba400034e423afcba3a32cd41cf51d8d
Joe-Newm <j.newm500@gmail.com>
2025-12-10 20:43:53 -0600

    refactor main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 from package import Package
 from hashTable import HashTable
 
-def main():
+# function for creating table populated with packages.csv file
+def createTable():
     table = HashTable(10)
-
     # open and read packages file, instantiate package object, add package object to hash table
     with open("packages.csv") as file:
         lines = file.readlines()
@@ -18,13 +18,34 @@ def main():
             while len(values) < 8:
                 values.append("")
             
-            
             #instantiate package object
             p = Package(*values)
             # add package to hash table
             table.insertItem(p)
+    return table
 
+# create list of lists with the distances floats from distances.csv
+def createDistanceData():
+    distances = []
+    with open("distances.csv") as file:
+        lines = file.readlines()
+
+        #loop through each line and make list of values on each row
+        for line in lines:
+            values = line.strip().split(",")
+
+            #append the list to the list of lists
+            distances.append(values)
+
+    return distances
+
+##################################################### main #####################################################
+
+def main():
+    table = createTable()
+    distances = createDistanceData()
     print(table)
+    print(distances[2][1])
 
 if __name__ == "__main__":
     main()
